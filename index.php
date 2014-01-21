2112e33c48a1de9e595abe21bb5afed8947fed8f
NJNJA <brunocanongia@gmail.com>
2014-01-21 19:10:05 -0200

    teste de escala

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 <html lang="pt-br">
 <head>
-	<title>Coconut - Crowd Funding</title>
+	<title>Coconut by JanjaDevTeam</title>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
@@ -10,3 +10,9 @@
 </head>
 
 <body>
+	<h2>Bem vindos a Coconut</h2>
+	<h3>Bem vindos a Coconut</h3>
+	<h4>Bem vindos a Coconut</h4>
+	<p>Bem vindos a Coconut</p>
+</body>
+</html>
