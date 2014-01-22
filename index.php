84e43e4727e41816d87cdd858eb74f5279e536d4
NJNJA <brunocanongia@gmail.com>
2014-01-22 18:26:26 -0200

    layout

--- index.php
+++ index.php
@@ -5,14 +5,23 @@
 	<title>Coconut by JanjaDevTeam</title>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"/>
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="css/main.css"/>
 	<link rel="stylesheet" type="text/css" href="css/janja.css"/>
 </head>
 
 <body>
-	<h2>Bem vindos a Coconut</h2>
-	<h3>Bem vindos a Coconut</h3>
-	<h4>Bem vindos a Coconut</h4>
-	<p>Bem vindos a Coconut</p>
+	<div class='container_100'>
+		<div id="logo-container" class='container_80'>
+			<div class='col-1-3'><img src='img/solucionatica.png' class='logo'/></div>
+			<div id="menu" class='col-2-3 text-r'><a href='#'><strong>Explorar &nbsp;&nbsp;-&nbsp;&nbsp; Enviar Projeto &nbsp;&nbsp;|&nbsp;&nbsp; Entrar &nbsp;&nbsp;-&nbsp;&nbsp; Registrar</strong></a></div>
+		</div>
+	</div>
+	<div id="banner" class='container_100 text-c'>
+		<h2>Coconut Crowd Engine</h2>
+		<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>
+	</div>
+	
+	
 </body>
 </html>
