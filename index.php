1fb225fdc103eed9cd89e20afb93cb5fa519839d
Bruno GanjaDevTeam <bruno@ganja.com>
2014-01-23 12:41:23 -0200

    mudança total no layout

--- index.php
+++ index.php
@@ -11,7 +11,41 @@
 </head>
 
 <body>
+<div id='top-borda' class='container-100'></div>
+<div class='container-80'>
+	<div id='logo-menu'>
+		<div id='logo'class='col-1-3'><img src='img/solucionatica.png' class='logo' alt='Solucionática'/></div>
+		<div class='col-2-3 text-r' id='menu'>
+			<ul>
+				<li><strong>EXPLORAR</strong></li>
+				<a href='#'><li><strong>ENVIAR PROJETO</strong></li></a>
+				<li class='selecionado'><strong>REGISTRAR</strong></li>
+				<li><strong>LOGAR</strong></li>
+			</ul>
+		</div>
+	</div>
+	<div id='destaques' class='container-100'>
+		<div class='col-1-3 pad-1 bbox text-c'>
+			<img src='img/tmp/01.jpg' class='destaque-img'/>
+			<p>PROJETOS PARA WEB</p>
+		</div>
+		<div class='col-1-3 pad-1 bbox text-c'>
+			<img src='img/tmp/02.jpg' class='destaque-img'/>
+			<p>PROJETOS PARA WEB</p>
+		</div><div class='col-1-3 pad-1 bbox text-c'>
+			<img src='img/tmp/03.jpg' class='destaque-img'/>
+			<p>PROJETOS PARA WEB</p>
+		</div>
+	</div>
+	<div class='text-c'><h4>ÚLTIMOS PROJETOS</h4></div>
 	
+	<div id='projetos'>
+		<div class='col-1-4'></div>
+		<div class='col-1-4'></div>
+		<div class='col-1-4'></div>
+		<div class='col-1-4'></div>
+	</div>
+</div>
 	
 </body>
 </html>
