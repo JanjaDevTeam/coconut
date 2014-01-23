677f70526d809b2b046c2b0344e85b0c31c336ac
NJNJA <brunocanongia@gmail.com>
2014-01-23 19:26:54 -0200

    refactory no layout

--- index.php
+++ index.php
@@ -13,8 +13,9 @@
 <body>
 <div id='top-borda' class='container-100'></div>
 <div class='container-80'>
+	
 	<div id='logo-menu'>
-		<div id='logo'class='col-1-3'><img src='img/solucionatica.png' class='logo' alt='Solucionática'/></div>
+		<div id='logo'class='col-1-3 bbox'><img src='img/solucionatica.png' class='logo' alt='Solucionática'/></div>
 		<div class='col-2-3 text-r' id='menu'>
 			<ul>
 				<li><a href='#'><strong>EXPLORAR</strong></a></li>
@@ -24,28 +25,57 @@
 			</ul>
 		</div>
 	</div>
-	<div id='destaques' class='container-100'>
-		<div class='col-1-3 pad-1 bbox text-c'>
+
+	<div id='banner'>
+		<div class='col-1-3-pad-1 bbox text-c'>
 			<img src='img/tmp/01.jpg' class='destaque-img'/>
 			<p>PROJETOS PARA WEB</p>
 		</div>
-		<div class='col-1-3 pad-1 bbox text-c'>
+		<div class='col-1-3-pad-1 bbox text-c'>
 			<img src='img/tmp/02.jpg' class='destaque-img'/>
 			<p>PROJETOS PARA WEB</p>
-		</div><div class='col-1-3 pad-1 bbox text-c'>
+		</div><div class='col-1-3-pad-1 bbox text-c'>
 			<img src='img/tmp/03.jpg' class='destaque-img'/>
 			<p>PROJETOS PARA WEB</p>
 		</div>
 	</div>
-	<div class='text-c'><h4>ÚLTIMOS PROJETOS</h4></div>
 	
 	<div id='projetos'>
-		<div class='col-1-4'></div>
-		<div class='col-1-4'></div>
-		<div class='col-1-4'></div>
-		<div class='col-1-4'></div>
+		<div class='text-c'><h4>ÚLTIMOS PROJETOS</h4></div>
+		<hr/>
+		
+		<div class='ultimos col-1-4-pad-1'>
+			<div class='container-100'><img src='img/tmp/proj01.jpg' alt='projeto'/></div>
+			<div class='container-100 pad-5 bbox'>
+				<p>PROJETO TAL</p>
+				<span class='small'>Descrição do projeto</span>
+			</div>
+		</div>
+		
+		<div class='ultimos col-1-4-pad-1'>
+			<div class='container-100'><img src='img/tmp/proj02.jpg' alt='projeto'/></div>
+			<div class='container-100 pad-5 bbox'>
+				<p>PROJETO TAL</p>
+			</div>
+		</div>
+		
+		<div class='ultimos col-1-4-pad-1'>
+			<div class='container-100'><img src='img/tmp/proj03.jpg' alt='projeto'/></div>
+			<div class='container-100 pad-5 bbox'>
+				<p>PROJETO TAL</p>
+			</div>
+		</div>
+		
+		<div class='ultimos col-1-4-pad-1'>
+			<div class='container-100'><img src='img/tmp/proj04.jpg' alt='projeto'/></div>
+			<div class='container-100 pad-5 bbox'>
+				<p>PROJETO TAL</p>
+			</div>
+		</div>
 	</div>
+	<br/>
+	<hr/>
+	<br/>
 </div>
-	
 </body>
 </html>
