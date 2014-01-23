f454355412330393e9eff0bd456ed68e06c49339
NJNJA <brunocanongia@gmail.com>
2014-01-23 10:46:07 -0200

    mudança de direção

--- index.php
+++ index.php
@@ -11,16 +11,6 @@
 </head>
 
 <body>
-	<div class='container_100'>
-		<div id="logo-container" class='container_80'>
-			<div class='col-1-3'><img src='img/solucionatica.png' class='logo'/></div>
-			<div id="menu" class='col-2-3 text-r'><a href='#'><strong>Explorar &nbsp;&nbsp;-&nbsp;&nbsp; Enviar Projeto &nbsp;&nbsp;|&nbsp;&nbsp; Entrar &nbsp;&nbsp;-&nbsp;&nbsp; Registrar</strong></a></div>
-		</div>
-	</div>
-	<div id="banner" class='container_100 text-c'>
-		<h2>Coconut Crowd Engine</h2>
-		<p>Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. </p>
-	</div>
 	
 	
 </body>
