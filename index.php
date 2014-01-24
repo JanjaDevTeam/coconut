7189fe7458ccc0fc4b8a94241fa9f4fbf7c15693
NJNJA <brunocanongia@gmail.com>
2014-01-24 18:00:39 -0200

    layout atualizado

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 <body>
 <div id='top-borda' class='container-100'></div>
-<div class='container-80'>
+<div class='container-90'>
 	
 	<div id='logo-menu'>
 		<div id='logo'class='col-1-3 bbox'><img src='img/solucionatica.png' class='logo' alt='Solucionática'/></div>
@@ -53,23 +53,25 @@
 				<div id='destaque-descricao'>
 					<span class='small'>Guide Bit for a side-scrolling adventure in a primitive world to help his new mysterious friend Ravh return home. </span>
 				</div>
+				<div class='financiado '><strong><span class='letra-escura'>75%</span> financiado</strong></div>
 				<div id='barra-container'>
 					<div id='barra' class='barra-75'></div>
 				</div>
 			</div>
 			<div id='info-container'>
 				<div class='letra-escura'>
-					<div class='col-1-3 bbox text-c'><strong>75%</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>R$120.000,00</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>15</strong></div>
+					
+					<div class='col-1-2 bbox text-c'><strong>R$120.000,00</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>15 dias</strong></div>
 				</div>
 				
-				<div class='col-1-3 bbox text-c'>concluido</div>
-				<div class='col-1-3 bbox text-c'>arrecadado</div>
-				<div class='col-1-3 bbox text-c'>dias restantes</div>
+				
+				<div class='col-1-2 bbox text-c'>levantado</div>
+				<div class='col-1-2 bbox text-c'>restantes</div>
 			
 			</div>
 		</div>
+		
 		
 		
 		
@@ -82,20 +84,19 @@
 				<div id='destaque-descricao'>
 					Imagine a world cut out of paper- walls that want to breath, & shadows dancing with the floor. 
 				</div>
+				<div class='financiado '><strong><span class='letra-escura'>33%</span> financiado</strong></div>
 				<div id='barra-container'>
 					<div id='barra' class='barra-33'></div>
 				</div>
 			</div>
 			<div id='info-container'>
 				<div class='letra-escura'>
-					<div class='col-1-3 bbox text-c'><strong>33%</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>R$250,00</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>15</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>R$250,00</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>12 dias</strong></div>
 				</div>
 				
-				<div class='col-1-3 bbox text-c'>concluido</div>
-				<div class='col-1-3 bbox text-c'>arrecadado</div>
-				<div class='col-1-3 bbox text-c'>dias restantes</div>
+				<div class='col-1-2 bbox text-c'>levantado</div>
+				<div class='col-1-2 bbox text-c'>restantes</div>
 			
 			</div>
 		</div>
@@ -112,20 +113,19 @@
 				<div id='destaque-descricao'>
 					We´re proud of we´ve done in a short time but now it´s time of truth. The biggest failure of all? Never trying. Spoh, the Chilean beer.
 				</div>
+				<div class='financiado '><strong><span class='letra-escura'>10%</span> financiado</strong></div>
 				<div id='barra-container'>
 					<div id='barra' class='barra-10'></div>
 				</div>
 			</div>
 			<div id='info-container'>
 				<div class='letra-escura'>
-					<div class='col-1-3 bbox text-c'><strong>10%</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>R$3.000,00</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>15</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>R$3.000,00</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>5 dias</strong></div>
 				</div>
 				
-				<div class='col-1-3 bbox text-c'>concluido</div>
-				<div class='col-1-3 bbox text-c'>arrecadado</div>
-				<div class='col-1-3 bbox text-c'>dias restantes</div>
+				<div class='col-1-2 bbox text-c'>levantado</div>
+				<div class='col-1-2 bbox text-c'>restantes</div>
 			
 			</div>
 		</div>
@@ -141,29 +141,54 @@
 				<div id='destaque-descricao'>
 					Help us fund the remake of our acclaimed Fan Movie STALKER: Monolith's Whisper or get out of here Stalker! 
 				</div>
+				<div class='financiado '><strong><span class='letra-escura'>90%</span> financiado</strong></div>
 				<div id='barra-container'>
 					<div id='barra' class='barra-90'></div>
 				</div>
 			</div>
 			<div id='info-container'>
 				<div class='letra-escura'>
-					<div class='col-1-3 bbox text-c'><strong>90%</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>R$30.000,00</strong></div>
-					<div class='col-1-3 bbox text-c'><strong>15</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>R$30.000,00</strong></div>
+					<div class='col-1-2 bbox text-c'><strong>3 dias</strong></div>
 				</div>
-				
-				<div class='col-1-3 bbox text-c'>concluido</div>
-				<div class='col-1-3 bbox text-c'>arrecadado</div>
-				<div class='col-1-3 bbox text-c'>dias restantes</div>
+				<div class='col-1-2 bbox text-c'>levantado</div>
+				<div class='col-1-2 bbox text-c'>restantes</div>
 			
 			</div>
 		</div>
 	</div>
-	
+</div>
+<div id='popular'class='container-100 debug'>
+	<div class='container-80'>
 
-	<div id='concluidos' class='col-1'>
-		<div class='text-c'><h4>PROJETOS CONCLUÍDOS</h4></div>
-		<hr/>
+		<div id='concluidos' class='col-1'>
+			<div class='text-c'><h4>ENCONTRE</h4></div>
+			<hr/>
+		</div>
+		
+		<div id='aleatorios' class='col-2-3'>
+			<div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/01.jpg' alt='projeto'/></div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/02.jpg' alt='projeto'/></div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/03.jpg' alt='projeto'/></div>
+			</div>
+			<br/><br/><br/><br/><br/><br/><br/><br/>
+			<div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/01.jpg' alt='projeto'/></div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/02.jpg' alt='projeto'/></div>
+				<div class='col-1-3-pad-1'><img src='img/tmp/03.jpg' alt='projeto'/></div>
+			</div>
+		</div>
+		<div id='busca-principal'class='col-1-3 text-c bbox'>
+		
+		
+		<!-- busca -->
+		<form action='#' method='post'>
+			<input type="text" name="busca" class='bbox'/>
+		</form>
+		<!-- /busca -->
+		
+		</div>
 	</div>
 </div>
 </body>
