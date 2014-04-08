b1636c0ca62fd260777a74869057536c8b2c3ebe
NJNJA <brunocanongia@gmail.com>
2014-04-08 18:42:01 -0300

    footer separado

--- view/template/main.php
+++ view/template/main.php
@@ -32,21 +32,9 @@
 	
 </div>
 
-<div id='footer' class='container-100 bg-footer clear-b bbox'>
-	<div class='container-80'>
-		<div class='text-c bbox'>
-			<img src='img/solucionatica-footer.png' alt='Solucionática'/>
-			<ul>
-				<li>COMO FUNCIONA?</li>
-				<li>FAQ/AJUDA</li>
-				<li>TERMOS DE USO</li>
-				<li>PRIVACIDADE</li>
-				<li>CONTATO</li>
-			</ul>
-		</div>
-		
-	</div>
-</div>
+<?php
+require_once('view/template/footer.php');
+?>
 </div>
 
 </body>
